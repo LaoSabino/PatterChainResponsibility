0ea712a461879619a7ace533d5d582495cc66c5b
sabinonet <laerc@JRSABINONET>
2024-12-23 23:45:44 -0300

    Testes

---
+++ Estudo.CadeiaResponsabilidade.Teste/Test1.cs
@@ -0,0 +1,27 @@
+﻿using Estudo.CadeiaResponsabilidade.Handler;
+using Estudo.CadeiaResponsabilidade.Interface;
+using Estudo.CadeiaResponsabilidade.Model;
+using Estudo.CadeiaResponsabilidade.Service;
+
+namespace Estudo.CadeiaResponsabilidade.Teste;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void Test_Status_Emissao()
+    {
+        IEmissaoService emissaoService = new EmissaoService();
+        var pedido = new Pedido { Status = StatusType.Emissao };
+        var analiseRiscoHandler = new AnaliseRiscoHandler();
+        var initHandler = new InitHandler();
+        var cancelamentoHandler = new CancelamentoHandler();
+        var emissaoHandler = new EmissaoHandler(emissaoService);
+
+        initHandler.SetNext(analiseRiscoHandler);
+        analiseRiscoHandler.SetNext(emissaoHandler);
+        emissaoHandler.SetNext(cancelamentoHandler);
+        initHandler.Handle(pedido);
+
+    }
+}
